3eb7d8ea37a70c3a96f513ef6aec473b548d792e
Michelle Wehr <michelle_wehr@icloud.com>
2021-08-18 19:45:56 -0400

    defined api url, passed api url into fetch method then function passed response into the function then response.json. then function(data) console logged data. got my own repos in console when ran getUserRepos('michellewehr')

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,16 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response){
+        //response object in fetch logic has method called json() this method formats the response as json but sometimes a resource may return non-json data. in those cases a different method like text() would be used
+        //json method returns another promise- hence the extra then() method whos callback function captured actual data
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    });
 };
-getUserRepos();
+
+//(outside is console logged first before the fetch() response because asynchronus and takes longer to do fetch response so keeps running and comes back to it)=> referred to as AJAX = asynchronous javascript and xml=> xml = term refers to old fashioned way of formatting data- replaced by JSON
+getUserRepos("michellewehr");
