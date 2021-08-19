483dfd3db3a6edefeffd6fba5d49e5c0a66ffb33
Michelle Wehr <michelle_wehr@icloud.com>
2021-08-19 18:34:44 -0400

    get repo/ issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit "; 
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,7 +21,11 @@ var getRepoIssues = function(repo) {
             //pass response data to dom function
             response.json().then(function(data) {
                 displayIssues(data);
+                if(response.headers.get("Link")) {
+                    displayWarning();
+                }
             })
+
         } else {
             alert("There was a problem with your request!");
         }
